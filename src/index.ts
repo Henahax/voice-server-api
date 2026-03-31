70c39ea1e5fa4dd40f7b92969e9518115b5f9ab1
Henahax <your_private_email@users.noreply.github.com>
2026-04-01 01:05:00 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes';
 
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(helmet());
+app.use(cors()); // Enable CORS for all origins
 app.use(express.json());
 
 app.use('/api', router); // all routes are prefixed with /api
